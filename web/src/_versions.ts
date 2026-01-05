552d336df4a7b820c089d94e1235c0b4d01bca94
MMolineux <matthew.molineux@protonmail.com>
2026-01-05 12:02:11 +1000

    add web ui

--- web/src/_versions.ts
+++ web/src/_versions.ts
@@ -11,6 +11,8 @@ export interface TsAppVersion {
 export const versions: TsAppVersion = {
     version: '0.0.0',
     name: 'opentakserver-ui',
-    versionDate: '2026-01-04T00:45:58.262Z',
+    versionDate: '2026-01-05T01:26:42.364Z',
+    gitCommitHash: '1062a5a',
+    versionLong: '0.0.0-1062a5a',
 };
 export default versions;
